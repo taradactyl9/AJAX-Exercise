9cd5c4f5449c6b0a97e7e1ca308dcb060bdca02a
Tara Davies <taradavies@Taras-Air.attlocal.net>
2020-10-06 21:02:03 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("generate");
+
+button.addEventListener("click", function() {
+    button.innerHTML = "Generating Doggo...";
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response){
+             return response.json();
+        })
+        .then(function(data){
+            let dogImage = document.createElement('img');
+            dogImage.setAttribute('src', data.message);
+            document.body.appendChild(dogImage);
+            button.innerHTML = "Generate Doggo";
+        })
+    })
